94184337c2f3f5eeedfb415a55334258f8faf821
briansak <brsak@cisco.com>
2015-01-22 12:52:10 -0600

    Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This fuction attempts to cache content that it time-consuming to compute so that it can be reused without
+## incurring the time penalty to recompute a value.
 
-## Write a short comment describing this function
+## makeCacheMatrix function
+## Creates a matrix to contain the cached data.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matrix <- NULL
+  get <- function () x
+  setInverseMatrix <- function (inverse) matrix <<- inverse
+  getInverseMatrix <- function() matrix
+  
+  list (get = get, setInverseMatrix = setInverseMatrix, getInverseMatrix = getInverseMatrix)
 }
 
+## cacheSolve function
+## This function checks to see if the content already exists and if it does, the function exits.  If not, it will
+## be stored in a matrix.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  matrix <- x$getInverseMatrix()
+  if(!is.null(matrix)){
+    message("Cached data found.")
+    return(matrix)
+  }
+  else {
+    message("No cached data found")
+    data <- x$get() 
+    matrix <- solve(data)
+    x$setInverseMatrix(matrix)
+    message("Loading of Inverse Matrix Complete.")
+    return(m)
+  }
 }
